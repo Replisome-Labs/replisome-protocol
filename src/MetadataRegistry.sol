aac33c9caa8ba6c78cd76af4ac109931b1102274
HSIAO PO WEN <xiawpohr@gmail.com>
2022-08-13 17:13:47 +0800

    test MetadataRegistry

--- src/MetadataRegistry.sol
+++ src/MetadataRegistry.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.13;
 import {AlreadyRegisteredMetadata, NotRegisteredMetadata} from "./interfaces/Errors.sol";
 import {IMetadataRegistry} from "./interfaces/IMetadataRegistry.sol";
 import {IMetadata} from "./interfaces/IMetadata.sol";
+import {Owned} from "./libraries/Owned.sol";
 
-contract MetadataRegistry is IMetadataRegistry {
+contract MetadataRegistry is Owned(msg.sender), IMetadataRegistry {
     mapping(IMetadata => bool) public isRegistered;
 
-    function register(IMetadata metadata) external {
+    function register(IMetadata metadata) external onlyOwner {
         if (isRegistered[metadata]) {
             revert AlreadyRegisteredMetadata(metadata);
         }
@@ -16,7 +17,7 @@ contract MetadataRegistry is IMetadataRegistry {
         emit Registered(metadata);
     }
 
-    function unregister(IMetadata metadata) external {
+    function unregister(IMetadata metadata) external onlyOwner {
         if (!isRegistered[metadata]) {
             revert NotRegisteredMetadata(metadata);
         }
---
+++ test/MetadataRegistry.t.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {MetadataRegistry} from "../src/MetadataRegistry.sol";
+import {IMetadata} from "../src/interfaces/IMetadata.sol";
+import {Unauthorized, AlreadyRegisteredMetadata, NotRegisteredMetadata} from "../src/interfaces/Errors.sol";
+
+contract MetadataRegistryTest is Test {
+    using stdStorage for StdStorage;
+
+    event Registered(IMetadata indexed metadata);
+
+    event Unregistered(IMetadata indexed metadata);
+
+    MetadataRegistry public metadataRegistry;
+
+    address public constant prankAddress = address(0);
+    IMetadata public constant mockMetadata = IMetadata(address(100));
+
+    function setUp() public {
+        metadataRegistry = new MetadataRegistry();
+    }
+
+    function testRegister() public {
+        vm.expectEmit(true, false, false, false);
+        emit Registered(mockMetadata);
+        metadataRegistry.register(mockMetadata);
+        assertEq(metadataRegistry.isRegistered(mockMetadata), true);
+    }
+
+    function testRegisterTwice() public {
+        stdstore
+            .target(address(metadataRegistry))
+            .sig(metadataRegistry.isRegistered.selector)
+            .with_key(address(mockMetadata))
+            .checked_write(true);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AlreadyRegisteredMetadata.selector,
+                mockMetadata
+            )
+        );
+        metadataRegistry.register(mockMetadata);
+    }
+
+    function testRegisterAsNotOwner() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Unauthorized.selector, prankAddress)
+        );
+        vm.startPrank(prankAddress);
+        metadataRegistry.register(mockMetadata);
+        vm.stopPrank();
+    }
+
+    function testUnregister() public {
+        stdstore
+            .target(address(metadataRegistry))
+            .sig(metadataRegistry.isRegistered.selector)
+            .with_key(address(mockMetadata))
+            .checked_write(true);
+        vm.expectEmit(true, false, false, false);
+        emit Unregistered(mockMetadata);
+        metadataRegistry.unregister(mockMetadata);
+        assertEq(metadataRegistry.isRegistered(mockMetadata), false);
+    }
+
+    function testUnregisterTwice() public {
+        stdstore
+            .target(address(metadataRegistry))
+            .sig(metadataRegistry.isRegistered.selector)
+            .with_key(address(mockMetadata))
+            .checked_write(false);
+        vm.expectRevert(
+            abi.encodeWithSelector(NotRegisteredMetadata.selector, mockMetadata)
+        );
+        metadataRegistry.unregister(mockMetadata);
+    }
+
+    function testUnregisterAsNotOwner() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Unauthorized.selector, prankAddress)
+        );
+        vm.startPrank(prankAddress);
+        metadataRegistry.unregister(mockMetadata);
+        vm.stopPrank();
+    }
+}
